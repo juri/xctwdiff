b3d94fe88414de6211b9baa8a1475c4d9ae0bc70
Juri Pakaste <juri@iki.fi>
2018-06-24 08:43:56 +0300

    Initial Commit

---
+++ xctwdiff/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  xctwdiff
+//
+//  Created by Juri Pakaste on 24/06/2018.
+//  Copyright © 2018 Juri Pakaste. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
